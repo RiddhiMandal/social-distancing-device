b9e3ed59aa0933b764177ac6178e9905ec8846fb
RiddhiMandal <Riddhimandal1814@gmail.com>
2022-05-27 12:07:19 +0530

    Update meu_project.ino

--- meu_project.ino
+++ meu_project.ino
@@ -5,6 +5,10 @@
 #include <BLEServer.h>
 int buzzer = 4 ;
 int LED = 2 ;
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
 #define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 int scanTime = 1;     // In seconds
@@ -36,6 +40,19 @@ class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
   }
 };
 
+/*Create a BLE server that, once we receive a connection, will send periodic notifications.
+   The service advertises itself as: 
+   And has a characteristic of: 
+   The design of creating the BLE server is:
+   1. Create a BLE Server
+   2. Create a BLE Service
+   3. Create a BLE Characteristic on the Service
+   4. Create a BLE Descriptor on the characteristic
+   5. Start the service.
+   6. Start advertising.
+   A connect hander associated with the server starts a background task that performs notification
+   every couple of seconds.*/
+     
 void setup()
 {
   Serial.begin(115200);
@@ -51,9 +68,9 @@ void setup()
           BLECharacteristic::PROPERTY_WRITE);
 
   pCharacteristic->setValue("MEU");
-  pService->start();
+  pService->start();   // restart advertising
   
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising(); //Called when a new scan result is detected
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(true);
   BLEDevice::startAdvertising();
@@ -62,7 +79,7 @@ void setup()
   Serial.println("Scanning...");
   pinMode(buzzer, OUTPUT); // set pin to output mode
   BLEDevice::init("MEU");
-  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());  //Called when a new scan result is detected.
   pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
   pBLEScan->setInterval(100);
   pBLEScan->setWindow(99); // less or equal setInterval value
